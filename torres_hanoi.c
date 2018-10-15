8d66e9b3a9961921d87fb4ae78b6de5b87a82973
santiago <santiago@aparello01.villarleal.lan>
2018-10-15 14:39:35 +0200

    Engado versión offline, devolve a solución óptima

--- torres_hanoi.c
+++ torres_hanoi.c
@@ -2,20 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ERR_SACAR_TOR_BALEIRA -4
-#define ERR_METER_ELEM_GRANDE -3
-#define ERR_METER_TOR_CHEA    -2
-#define ERR_METER_ELEM_0      -1
-#define OK                     0
-#define ERR_USO                1
+#define ERR_MOVER_ELEM_OFFLINE -5
+#define ERR_SACAR_TOR_BALEIRA  -4
+#define ERR_METER_ELEM_GRANDE  -3
+#define ERR_METER_TOR_CHEA     -2
+#define ERR_METER_ELEM_0       -1
+#define OK                      0
+#define ERR_USO                 1
 
-#define TRUE                   1
-#define FALSE                  0
-#define TAM_MAX_OPC           50
+#define TRUE                    1
+#define FALSE                   0
+#define TAM_MAX_OPC            50
+
+#define TORRE_1 "torre 1"
+#define TORRE_2 "torre 2"
+#define TORRE_3 "torre 3"
 
 void imprimir_uso(char *prog)
 {
-  printf("%s <N>\n", prog);
+  printf("%s <N> [-b|-i]\n", prog);
+  printf("N: número de elementos.\n");
+  printf("-b: offline, imprime a solución.\n");
+  printf("-i: online, permite ao usuario buscar a solución.\n");
+  printf("Por defecto a execución será online.\n");
+  exit(ERR_USO);
+}
+
+unsigned int nome_torre_to_int(char *nome_torre)
+{
+  if (!strcmp(nome_torre, TORRE_1)) return 1;
+  else if (!strcmp(nome_torre, TORRE_2)) return 2;
+  else if (!strcmp(nome_torre, TORRE_3)) return 3;
+  else return 0;
 }
 
 void zeros(unsigned int *torre, unsigned int tam)
@@ -48,7 +66,8 @@ unsigned char torre_chea(unsigned int *torre, unsigned int tam)
 
 int sacar(unsigned int *torre, unsigned int tam)
 {
-  int elemento = 0, i=0;
+  int elemento = 0;
+  unsigned int i = 0;
   if (torre_baleira(torre)) return ERR_SACAR_TOR_BALEIRA;
   elemento=torre[0];
   for (i=1;(i<tam)||(torre[i]);i++)
@@ -109,7 +128,8 @@ void ler_torre(char *nome_torre, unsigned int *torre, unsigned int tam)
 
 void axuda()
 {
-  printf("valores validos:\n");
+  printf("O obxetivo é mover os elementos da torre 1 á torre 2, usando como torre auxiliar a torre 3.\n");
+  printf("Valores validos:\n");
   printf("A-B: mover de torre A a torre B, sendo A,B={1,2,3}. Exemplos: 1-2, 3-1,...\n");
   printf("fora: acaba o xogo.\n\n");
 }
@@ -117,9 +137,9 @@ void axuda()
 void ler_torres(unsigned int *torre1, unsigned int *torre2, unsigned int *torre3, unsigned int tam)
 {
   printf("Estado actual: \n");
-  ler_torre("torre1", torre1, tam);
-  ler_torre("torre2", torre2, tam);
-  ler_torre("torre3", torre3, tam);
+  ler_torre(TORRE_1, torre1, tam);
+  ler_torre(TORRE_2, torre2, tam);
+  ler_torre(TORRE_3, torre3, tam);
 }
 
 unsigned char torre_valida(char t)
@@ -127,26 +147,14 @@ unsigned char torre_valida(char t)
   return ((t=='1')||(t=='2')||(t=='3'));
 }
 
-int main(int argc, char *argv[])
+void xogar(unsigned int *torre1,
+           unsigned int *torre2,
+           unsigned int *torre3,
+           unsigned int n)
 {
-  unsigned int n = 0, *torre1, *torre2, *torre3, *torreA, *torreB;
-  int res_mover=0;
   char opc[TAM_MAX_OPC];
-
-  if (argc!=2)
-  {
-    imprimir_uso(argv[0]);
-    exit(ERR_USO);
-  }
-
-  n = atoi(argv[1]);
-  torre1=(unsigned int *)malloc(sizeof(unsigned int )*(n));
-  torre2=(unsigned int *)malloc(sizeof(unsigned int )*(n));
-  torre3=(unsigned int *)malloc(sizeof(unsigned int )*(n));
-
-  inicializar(torre1, torre2, torre3, n);
-  axuda();
-  ler_torres(torre1, torre2, torre3, n);
+  unsigned int *torreA, *torreB;
+  int res_mover=0;
 
   while (TRUE)
   {
@@ -197,8 +205,84 @@ int main(int argc, char *argv[])
       printf("Acabache.\n");
     }
   }
+}
+
+void xogo_online(unsigned int n)
+{
+  unsigned int *torre1, *torre2, *torre3;
+
+  torre1=(unsigned int *)malloc(sizeof(unsigned int )*(n));
+  torre2=(unsigned int *)malloc(sizeof(unsigned int )*(n));
+  torre3=(unsigned int *)malloc(sizeof(unsigned int )*(n));
+
+  inicializar(torre1, torre2, torre3, n);
+  axuda();
+  ler_torres(torre1, torre2, torre3, n);
+
+  xogar(torre1, torre2, torre3, n);
+
   free(torre1);
   free(torre2);
   free(torre3);
+}
+
+void mover_elem_offline(unsigned int n,
+                char *torre_orix,
+                char *torre_dest)
+{
+  unsigned int torre_orix_int = nome_torre_to_int(torre_orix);
+  unsigned int torre_dest_int = nome_torre_to_int(torre_dest);
+  if ((!torre_orix_int)||(!torre_dest_int))
+  {
+    printf("Error mover_elem(..): torre non válida.\n");
+    exit(ERR_MOVER_ELEM_OFFLINE);
+  }
+  printf("%u-%u\n", torre_orix_int, torre_dest_int);
+}
+
+void hanoi_rec(unsigned int n,
+               char *nome_torre_orix,
+               char *nome_torre_dest,
+               char *nome_torre_aux)
+{
+  if (n==1)
+  {
+    mover_elem_offline(n, nome_torre_orix, nome_torre_dest);
+    return;
+  }
+  else
+  {
+    hanoi_rec(n-1, nome_torre_orix, nome_torre_aux, nome_torre_dest);
+  }
+  mover_elem_offline(n, nome_torre_orix, nome_torre_dest);
+  hanoi_rec(n-1, nome_torre_aux, nome_torre_dest, nome_torre_orix);
+}
+
+void xogo_offline(unsigned int n)
+{
+  hanoi_rec(n, TORRE_1, TORRE_2, TORRE_3);
+}
+
+int main(int argc, char *argv[])
+{
+  unsigned int n = 0;
+  unsigned char online = TRUE;
+
+  if ((argc!=2) && (argc!=3)) imprimir_uso(argv[0]);
+  if (argc==3)
+  {
+    if ((strlen(argv[2])==2) && (argv[2][0]=='-'))
+    {
+      if (argv[2][1]=='i') online = TRUE;
+      else if (argv[2][1]=='b') online = FALSE;
+      else imprimir_uso(argv[0]);
+    }
+    else imprimir_uso(argv[0]);
+  }
+
+  n = atoi(argv[1]);
+  if (online) xogo_online(n);
+  else xogo_offline(n);
+
 }
 
